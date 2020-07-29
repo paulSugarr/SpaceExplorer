d9a427b7d1dc972a22dd671f5d920dfd0cda06a0
Vlad <golublad@mail.ru>
2020-07-29 11:31:57 +0800

    Начал делать сущность игрока, добавил простое управление. Добавил тестовую сцену.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int _health = 100;
+    public Rigidbody PlayerRigidbody { get; private set; }
+    public float MoveSpeed { get; private set; }
+    
+    public Player (int health, float moveSpeed, Rigidbody playerRigidbody)
+    {
+        _health = health;
+        MoveSpeed = moveSpeed;
+        PlayerRigidbody = playerRigidbody;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        if (damage >= _health)
+        {
+            //Событие убийства
+            _health = 0;
+        }
+        else
+        {
+            _health -= damage;
+            //Событе нанесения дамага
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]private int health;
+    [SerializeField]private float moveSpeed;
+    [SerializeField]private Rigidbody playerRigidbody;
+    public Player Player;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        Player = new Player(health, moveSpeed, playerRigidbody);
+    }
+
+    private void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f ,Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * Player.MoveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Player.PlayerRigidbody.velocity = moveVelocity;
+    }
+}
