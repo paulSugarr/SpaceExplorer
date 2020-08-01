1b19b67f3b4467707cdc07f3284b2fe00bd5fec7
Paul <paul.sugar.a@gmail.com>
2020-08-01 15:55:31 +0400

    Добавил кнопку стрельбы

---
+++ Assets/Scripts/Setups/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singletone
+    private static GameManager _instance;
+    public static GameManager GetInstance()
+    {
+        if (_instance != null)
+        {
+            return _instance;
+        }
+        else
+        {
+            _instance = FindObjectOfType<GameManager>();
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    [SerializeField] private GameObject _player;
+    public static GameObject Player { get => GetInstance()._player; private set => GetInstance()._player = value; }
+
+    public static void RunCoroutine(IEnumerator enumerator)
+    {
+        GetInstance().StartCoroutine(enumerator);
+    }
+
+    public static void LoadScene(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+    public static void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/Scripts/UI/ShootButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private PlayerShooting _player;
+    private bool _isPressed = false;
+    private void Start()
+    {
+        _player = GameManager.Player.GetComponent<PlayerShooting>();
+    }
+    private void Update()
+    {
+        if (_isPressed)
+        {
+            _player.Shoot();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isPressed = false;
+    }
+}
